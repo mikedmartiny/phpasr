5cfb74d85dc01996d6da73d0769ce0470f8cf28d
Thomas Palmer <thomas.palmer@cigroup.co.uk>
2013-02-13 19:55:19 +0000

    Folder Structure

---
+++ lib/app.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package phpasr
+ * @author Thomas Palmer
+ * @version v1.0.0 13/02/2013 NEW FILE
+ */
+
+class app {
+	public function __construct() {
+		$this->appInit();
+	}
+
+	public function appInit() {
+		echo 1;
+	}
+}
---
+++ web/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package phpasr
+ * @author Thomas Palmer
+ * @version v1.0.0 13/002/213 NEW FILE
+ */
+
+require_once dirname(__FILE__).'/../lib/app.class.php';
+new app();
